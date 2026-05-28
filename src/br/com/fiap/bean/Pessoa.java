6e100162928c1ad3a1d3dfd8e9c33e4eda013e28
João Vitor P.R. <jvprocha.05gmail.com>
2026-05-28 17:21:24 -0300

    Inicializando Classe Pessoa

--- src/br/com/fiap/bean/Pessoa.java
+++ src/br/com/fiap/bean/Pessoa.java
@@ -1,4 +1,33 @@
 package br.com.fiap.bean;
 
 public class Pessoa {
+    //Atributos Pessoa
+    private String nome;
+    private int idade;
+    private String nacionalidade;
+
+    //Criando getters e setters
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    //Criando metodos
+    public String exibirDados() {
+        return null;
+    }
 }
