78260aa2ccf0da08e5786649e872da936c2a0ee0
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-05-29 18:54:30 -0300

    add: atributos da classe missao

--- src/br/com/fiap/bean/Missao.java
+++ src/br/com/fiap/bean/Missao.java
@@ -1,4 +1,9 @@
 package br.com.fiap.bean;
 
 public class Missao {
+    private String nomeMissao;
+    private String destino;
+    private int duracaoPrevistaDias;
+    private Status_Missao status;
+    private Astronauta[] tripulacao;
 }
