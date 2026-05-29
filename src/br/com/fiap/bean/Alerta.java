debe1b1f23a5bdfdef055136701e9208af2e34b9
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-05-29 18:23:36 -0300

    add: atributos e getters/setters da classe Alerta

--- src/br/com/fiap/bean/Alerta.java
+++ src/br/com/fiap/bean/Alerta.java
@@ -1,4 +1,43 @@
 package br.com.fiap.bean;
 
+import java.time.LocalDate;
+
 public class Alerta {
+
+    private Tipo_Alerta tipo;
+    private String descricao;
+    private Gravidade_Alerta nivelGravidade;
+    private LocalDate dataHora;
+    private boolean resolvido = false;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Gravidade_Alerta getNivelGravidade() {
+        return nivelGravidade;
+    }
+
+    public Tipo_Alerta getTipo() {
+        return tipo;
+    }
+
+    public LocalDate getDataHora() {
+        return dataHora;
+    }
+
+    public boolean isResolvido() {
+        return resolvido;
+    }
+
+    public void setResolvido(boolean resolvido) {
+        this.resolvido = resolvido;
+    }
+
+    public Alerta(Tipo_Alerta tipo, String descricao, Gravidade_Alerta gravidade){
+        this.tipo = tipo;
+        this.descricao = descricao;
+        this.nivelGravidade = gravidade;
+        this.dataHora = LocalDate.now();
+    }
 }
