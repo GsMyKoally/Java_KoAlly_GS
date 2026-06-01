2b0a8078261b9109c6d6c37b3c1bd62c7b4c86e9
João Vitor P.R. <jvprocha.05gmail.com>
2026-06-01 18:54:42 -0300

    add elementos da classe HistoricoSaude

--- src/br/com/fiap/bean/HistoricoSaude.java
+++ src/br/com/fiap/bean/HistoricoSaude.java
@@ -1,4 +1,97 @@
 package br.com.fiap.bean;
 
-public class HistoricoSaude {
+import java.util.List;
+
+public class HistoricoSaude implements Analisavel{
+    //Criando atributos
+    private List<SessaoMental> sessoes;
+    private int totalSessoes;
+    private Astronauta astronauta;
+
+    //Criando construtores
+    public HistoricoSaude(){}
+    public HistoricoSaude(List<SessaoMental> sessoes, int totalSessoes, Astronauta astronauta) {
+        this.sessoes = sessoes;
+        this.totalSessoes = totalSessoes;
+        this.astronauta = astronauta;
+    }
+
+    //Criando getters e setters
+    public List<SessaoMental> getSessoes() {
+        return sessoes;
+    }
+
+    public void setSessoes(List<SessaoMental> sessoes) {
+        this.sessoes = sessoes;
+    }
+
+    public int getTotalSessoes() {
+        return totalSessoes;
+    }
+
+    public void setTotalSessoes(int totalSessoes) {
+        this.totalSessoes = totalSessoes;
+    }
+
+    public Astronauta getAstronauta() {
+        return astronauta;
+    }
+
+    public void setAstronauta(Astronauta astronauta) {
+        this.astronauta = astronauta;
+    }
+
+
+    //Criando e importando metodos da classe
+    public void adicionarSessao(SessaoMental sessao){
+        sessoes.add(sessao);
+        totalSessoes++;
+    }
+
+    public double calcularMediaHumor(){
+        double soma = 0;
+        for(int i = 0; i < sessoes.size(); i++){
+            soma += sessoes.get(i).getNivelHumor();
+        }
+        if (totalSessoes == 0){
+            return 0;
+        }
+        else {
+            return soma / totalSessoes;
+        }
+    }
+
+    public String exibirRelatorioCompleto(){
+        String relatorio = "==== RELATÓRIO COMPLETO DE SESSÕES ====\n";
+        for (int i = 0; i < totalSessoes; i++){
+            relatorio += sessoes.get(i).exibirResumoSessao() + "\n";
+        }
+        return relatorio;
+    }
+
+    public String exibirRelatorioCompleto(int ultimas){
+        String relatorio = "==== RELATÓRIO COMPLETO DAS" + ultimas + " ÚLTIMAS SESSÕES ====\n";
+        int indice = totalSessoes - ultimas;
+        if (indice < 0){
+            indice = 0;
+        }
+        for (int i = indice; i < totalSessoes; i++){
+            relatorio += sessoes.get(i).exibirResumoSessao() + "\n";
+        }
+        return relatorio;
+    }
+
+    public String analisar() {
+        double media = calcularMediaHumor();
+        if (media < 5.0){
+            return "Atenção: O astronauta precisa de acompanhamento.";
+        }
+        else {
+            return "O astronauta está estável.";
+        }
+    }
+
+    public String gerarRelatorio() {
+        return exibirRelatorioCompleto();
+    }
 }
