ae35e2ebbc273fae62fdfa378915cab9ab3ecddf
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-06-01 16:39:05 -0300

    add: atributos e construtores da classe KoAlly

--- src/br/com/fiap/bean/KoAlly.java
+++ src/br/com/fiap/bean/KoAlly.java
@@ -1,4 +1,29 @@
 package br.com.fiap.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KoAlly {
+
+    //  ATRIBUTOS    //
+
+    private Missao missaoAtual;
+    private List<HistoricoSaude> historicos = new ArrayList<>();
+    private List<Alerta> alertasAtivos = new ArrayList<>();
+
+    //  CONSTRUTORES    //
+
+    public KoAlly(){}
+
+    public KoAlly(Missao missaoAtual, List<HistoricoSaude> historicos, List<Alerta> alertasAtivos){
+        this.missaoAtual = missaoAtual;
+        this.historicos = historicos;
+        this.alertasAtivos = alertasAtivos;
+    }
+
+    //  MÉTODOS //
+
+
+
+
 }
