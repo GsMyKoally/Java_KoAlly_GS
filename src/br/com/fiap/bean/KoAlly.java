d548d195f6fe80e91be2b9f1cf7da0b623469e06
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-06-01 16:40:36 -0300

    add: emissão de alerta

--- src/br/com/fiap/bean/KoAlly.java
+++ src/br/com/fiap/bean/KoAlly.java
@@ -29,6 +29,13 @@ public class KoAlly {
         return sess;
     }
 
+    public Alerta emitirAlerta(Tipo_Alerta tipo, String desc, Gravidade_Alerta grav){
+        return new Alerta(tipo,desc,grav);
+    }
+
+    public String analisarEstadoTripulacao(){
+        return "";
+    }
 
 
 }
