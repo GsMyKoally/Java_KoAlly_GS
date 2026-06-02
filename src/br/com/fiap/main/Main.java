1879ebf4f6b74299069905d40fe190f506e84e0b
João Vitor P.R. <jvprocha.05gmail.com>
2026-06-02 17:04:59 -0300

    add sistema de cadastro Main

--- src/br/com/fiap/main/Main.java
+++ src/br/com/fiap/main/Main.java
@@ -1,8 +1,12 @@
 package br.com.fiap.main;
 
+import br.com.fiap.bean.Astronauta;
+import br.com.fiap.bean.HistoricoSaude;
 import br.com.fiap.bean.KoAlly;
 import br.com.fiap.bean.Missao;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -12,22 +16,64 @@ public class Main {
         Missao missao = new Missao();
         String escolha = "sim", nomeMissao, destinoMissao, nome, nacionalidade, codAstronauta, funcao, sentimento, sessao, descricao;
         int opcao, duracaoDias, qtdAstronauta, idade, escolhaAstronauta, nivelHumor, escolhaTipo, escolhaGravidade, escolhaRelatorio, ultimas;
-        while(escolha.equalsIgnoreCase("sim")){
-            try {
-                System.out.println("==== BOAS-VINDAS AO KOALLY ====\nPara prosseguir informe os dados da missão");
-                System.out.println("Nome da missão: ");
-                nomeMissao = scan.next();
-                System.out.println("Destino da missão: ");
-                destinoMissao = scan.next();
-                System.out.println("Duração prevista da missão: ");
-                duracaoDias = scan.nextInt();
-                missao.setNomeMissao(nomeMissao);
-                missao.setDestino(destinoMissao);
-                missao.setDuracaoPrevistaDias(duracaoDias);
-                System.out.println(String.format("%s\n%s\n%d",missao.getNomeMissao(), missao.getDestino(), missao.getDuracaoPrevistaDias()));
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+
+        //Parte 1: Cadastro
+        try {
+            System.out.println("==== BOAS-VINDAS AO KOALLY ====\nPara prosseguir informe os dados da missão");
+            System.out.println("Nome da missão: ");
+            nomeMissao = scan.nextLine();
+            missao.setNomeMissao(nomeMissao);
+
+            System.out.println("Destino da missão: ");
+            destinoMissao = scan.nextLine();
+            missao.setDestino(destinoMissao);
+
+            System.out.println("Duração prevista da missão: ");
+            duracaoDias = scan.nextInt();
+            missao.setDuracaoPrevistaDias(duracaoDias);
+
+            System.out.println("Imforme a quantidade de astronautas que participarão da missão");
+            qtdAstronauta = scan.nextInt();
+            if (qtdAstronauta < 1){
+                System.out.println("ERRO: Quantidade inválida!");
             }
+            for (int i = 0; i < qtdAstronauta; i++){
+                System.out.println("==== Cadastro do Astronauta" + (i + 1) + "====");
+                System.out.println("Nome: ");
+                nome = scan.next();
+
+                System.out.println("Idade: ");
+                idade = scan.nextInt();
+
+                System.out.println("Nacionalidade: ");
+                nacionalidade = scan.next();
+
+                System.out.println("CodAstronauta: ");
+                codAstronauta = scan.next();
+
+                System.out.println("Função na nave:");
+                funcao = scan.next();
+                Astronauta astro = new Astronauta(nome, idade, nacionalidade, codAstronauta, funcao);
+                missao.adicionarTripulante(astro);
+
+                HistoricoSaude histor = new HistoricoSaude();
+                histor.setAstronauta(astro);
+                histor.setSessoes(new ArrayList<>());
+                koally.getHistoricos().add(histor);
+            }
+            missao.iniciarMissao();
+            koally.setMissaoAtual(missao);
+            System.out.println("==== Missão iniciada com êxito! ====");
+            System.out.println(missao.exibirDetalhesMissao());
+
+        } catch (Exception e) {
+            System.out.println("ERRO: Informação inserida está inválida!");;
+        }
+
+        //Partes 2: Menu principal
+        while(escolha.equalsIgnoreCase("sim")){
+
+
             System.out.println("Deseja Continuar (Sim|Não)?");
             escolha = scan.next();
         }
